cbdbba605fe4946c9d846c0c912c684948f7894b
voidgogo <2021038@daelim.ac.kr>
2025-09-19 08:09:09 +0900

    week03 : Greeting App

---
+++ AppWeek03/app/src/main/java/com/appweek03/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.appweek03
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+//class MainActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+//        setContentView(R.layout.activity_main)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+//    }
+//}
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val editTextName = findViewById<EditText>(R.id.editTextName)
+        val buttonGreet = findViewById<Button>(R.id.buttonGreet)
+        val textViewGreeting = findViewById<TextView>(R.id.textViewGreeting)
+
+        buttonGreet.setOnClickListener {
+            val name = editTextName.text.toString().trim()
+
+            if (name.isNotEmpty()) {
+                textViewGreeting.text = "안녕하세요, ${name}님!"
+                textViewGreeting.visibility = View.VISIBLE
+            } else {
+                textViewGreeting.text = "이름을 입력해주세요"
+                textViewGreeting.visibility = View.VISIBLE
+            }
+        }
+    }
+}
